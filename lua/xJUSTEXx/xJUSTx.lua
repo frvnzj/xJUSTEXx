d63d1d6889fd234f1478c487a67712d8dd43abb0
frvnzj <frvnzj.gmz@gmail.com>
2025-02-16 18:24:43 -0600

    refactor(xJUSTx): simplify code
    simplificando el código

--- lua/xJUSTEXx/xJUSTx.lua
+++ lua/xJUSTEXx/xJUSTx.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+--- Function to execute a "just" command with optional progress reporting
+---@param command string: The "just" command to execute
 function M.xCOMPILEx(command)
   local use_fidget = false
   local fidget
@@ -10,61 +12,63 @@ function M.xCOMPILEx(command)
     use_fidget = true
   end
 
+  local cmd = "just " .. command
+  local file_name = vim.fn.expand("%")
+  local handle
+
   if use_fidget then
-    local handle = fidget.progress.handle.create({
+    handle = fidget.progress.handle.create({
       title = "Just: " .. command,
-      message = "Compiling " .. vim.fn.expand("%") .. "...",
+      message = "Compiling " .. file_name .. "...",
       lsp_client = { name = "xJUSTEXx" },
       percentage = 0,
     })
-
-    local cmd = "just " .. command
-    vim.fn.jobstart(cmd, {
-      stdout_buffered = true,
-      on_stdout = function(_, data)
-        if data then
-          handle:report({ message = "Compiling " .. vim.fn.expand("%") .. "..." })
-        end
-      end,
-      on_stderr = function(_, data)
-        if data then
-          handle:report({ message = "Compiling " .. vim.fn.expand("%") .. "...", percentage = 50 })
-        end
-      end,
-      on_exit = function(_, code)
-        if code == 0 then
-          handle:finish()
-        else
-          handle:report({ message = "Failed" })
-          handle:finish()
-        end
-      end,
-    })
   else
     vim.notify("Starting compilation: " .. command, vim.log.levels.INFO)
-
-    local cmd = "just " .. command
-    vim.fn.jobstart(cmd, {
-      stdout_buffered = true,
-      on_stdout = function(_, data)
-        if data then
-          vim.notify("Compiling " .. vim.fn.expand("%") .. "...", vim.log.levels.INFO)
-        end
-      end,
-      on_stderr = function(_, data)
-        if data then
-          vim.notify("Compiling " .. vim.fn.expand("%") .. "... 50% done", vim.log.levels.WARN)
-        end
-      end,
-      on_exit = function(_, code)
-        if code == 0 then
-          vim.notify("Compilation finished successfully!", vim.log.levels.INFO)
-        else
-          vim.notify("Compilation failed!", vim.log.levels.ERROR)
-        end
-      end,
-    })
   end
+
+  vim.fn.jobstart(cmd, {
+    stdout_buffered = true,
+    on_stdout = function(_, data)
+      if data and #data > 0 then
+        local message = "Compiling " .. file_name .. "..."
+        if use_fidget then
+          handle:report({ message = message })
+        else
+          vim.notify(message, vim.log.levels.INFO)
+        end
+      end
+    end,
+    on_stderr = function(_, data)
+      if data and #data > 0 then
+        local message = "Compiling " .. file_name .. "... 50% done"
+        if use_fidget then
+          handle:report({ message = message, percentage = 50 })
+        else
+          vim.notify(message, vim.log.levels.WARN)
+        end
+      end
+    end,
+    on_exit = function(_, code)
+      if code == 0 then
+        local message = "Compilation finished successfully!"
+        if use_fidget then
+          handle:report({ message = message })
+          handle:finish()
+        else
+          vim.notify(message, vim.log.levels.INFO)
+        end
+      else
+        local message = "Compilation failed! Exit code: " .. code
+        if use_fidget then
+          handle:report({ message = message })
+          handle:finish()
+        else
+          vim.notify(message, vim.log.levels.ERROR)
+        end
+      end
+    end,
+  })
 end
 
 return M
